1b7dc7712e30e1d3d3633be3617fe72e2c5fd047
Amir Ali Anwar <amiralianwar611@gmail.com>
2022-10-08 11:39:55 +0500

    application completed

--- src/App.js
+++ src/App.js
@@ -21,9 +21,20 @@ function App() {
   if (loading) {
     return <Loading />;
   }
-  const { type, correct_answer, incorrect_answers, question} =
+  const { correct_answer, incorrect_answers, question} =
     questions[index];
-  const answers = [correct_answer, ...incorrect_answers];
+  // const answers = [correct_answer, ...incorrect_answers];
+  let answers= [...incorrect_answers]
+  const tempIndex= Math.floor(Math.random()* 3)
+  // console.log(answers.push(answers[tempIndex]));
+  //const answers= [1,2,3,]
+  console.log('values',answers[tempIndex])
+  if(tempIndex ===3){
+    answers.push(correct_answer)
+  }else{
+    answers.push(answers[tempIndex])
+    answers[tempIndex]=correct_answer
+  }
   return (
     <main>
       <Modal />
